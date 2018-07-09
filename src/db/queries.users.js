b399afacbd7ff0262dcb0e85afda2df6dd738a09
Mariel <mariel.capulli@gmail.com>
2018-07-09 14:55:14 -0400

    working on sendgrid

---
+++ src/db/queries.users.js
@@ -0,0 +1,32 @@
+const User = require("./models").User;
+const bcrypt = require("bcryptjs");
+
+
+module.exports = {
+    createUser(newUser, callback) {
+        const salt = bcrypt.genSaltSync();
+        const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+
+        return User.create({
+            username: newUser.username,
+            email: newUser.email,
+            password: hashedPassword
+        })
+        .then((user) => {
+            const sgMail = require("@sendgrid/mail");
+            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+            const msg = {
+                to: newUser.email,
+                from: 'donotreply@blocipedia.com',
+                subject: 'Blocipedia Account Confirmation',
+                text: 'Thank you for creating a Blocipedia account!',
+                html: '<strong>Log in now to start contributing!</strong>',
+              };
+              sgMail.send(msg);
+            callback(null, user);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    }
+}
