37b066d599f8da994c1e68c7810757a4441108b6
Mariel <mariel.capulli@gmail.com>
2018-07-10 06:46:53 -0400

    implemented sign-in/sign-out feature

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -11,7 +11,7 @@ module.exports = {
         const errors = req.validationErrors();
 
         if(errors){
-            req.flash("error", errorts);
+            req.flash("error", errors);
             return res.redirect(req.headers.referer); 
         } else {
             return next();
